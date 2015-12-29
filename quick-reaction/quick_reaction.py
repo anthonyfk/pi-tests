b4d8b11bb43bcd47f5fec58c9317a8a70548e24a
Kyle Fisher <anthonyfk@gmail.com>
2015-12-28 20:12:11 -0600

    Get the light part of the quick react game initially coded.

--- quick-reaction/quick_reaction.py
+++ quick-reaction/quick_reaction.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 from RPi import GPIO
 
 import time
+import random
 
+
+# Define our pins.
+class Pin(object):
+    LED = 4
+    BUTTON_A = 14
+    BUTTON_B = 15
+
+
+class QuickReact(object):
+
+    def __init__(self):
+        self.scores = defaultdict(int)
+        self.times = defaultdict(list)
+
+    def init_GPIO(self):
+        print("Initializing GPIO ...")
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(Pin.LED, GPIO.OUT)
+        GPIO.setup(Pin.BUTTON_A, GPIO.IN, GPIO.PUD_UP)
+        GPIO.setup(Pin.BUTTON_B, GPIO.IN, GPIO.PUD_UP)
+
+    def go(self):
+        # Blink light.
+        GPIO.output(Pin.LED, 1)
+        time.sleep(random.uniform(5, 10))
+        GPIO.output(Pin.LED, 0)
+
+        # Detect button.
+        # TODO
 
 
 def main():
-    # Initialize the GPIO module.
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-
-    print("something")
+    qr = QuickReact()
+    qr.init_GPIO()
+    qr.go()
 
 
 if __name__ == '__main__':
@@ -19,4 +50,3 @@ if __name__ == '__main__':
         main()
     finally:
         GPIO.cleanup()
-
